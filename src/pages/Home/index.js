33088fa1ce3fb56b73ca397df26f0de18ed2660a
Fabio Silva Ferreira <fabiosvf@gmail.com>
2019-12-22 21:47:18 -0300

    Módulo 'Arquitetura Flux', Vídeo 'Estilização da Home'

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,88 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-court-royale-suede/26/D12-2751-026/D12-2751-026_detalhe1.jpg?ims=240x240"
+          alt="Tênis"
+        />
+        <strong>
+          Tênis muito legal com título muito grande que quebra varias linhas
+        </strong>
+        <span>R$129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-court-royale-suede/26/D12-2751-026/D12-2751-026_detalhe1.jpg?ims=240x240"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-court-royale-suede/26/D12-2751-026/D12-2751-026_detalhe1.jpg?ims=240x240"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-court-royale-suede/26/D12-2751-026/D12-2751-026_detalhe1.jpg?ims=240x240"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-court-royale-suede/26/D12-2751-026/D12-2751-026_detalhe1.jpg?ims=240x240"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
